f09963a54ba136da5b3cd6603ea6578569e7a70c
Yousef_Badr <ybkk17@gmail.com>
2019-12-13 17:25:24 +0200

    Removed 7efnawy's game and put mine because it's better.

--- Source/Implement_SideCamera/Implement_SideCamera.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
-
-#include "Implement_SideCamera.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Implement_SideCamera, "Implement_SideCamera" );
- 
